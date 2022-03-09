f00d8985074869d485d7a687f472dd4e49641c2f
ARPIT GUPTA <ARPITGUPTA1014@gmail.com>
2022-03-09 15:13:18 +0530

    Adding php file

---
+++ Contactformhandler.php
@@ -0,0 +1,20 @@
+<?php
+    $name=$_POST['name'];
+    $visitor_email=$_POST['email'];
+    $subject=$_POST['subject'];
+    $message=$_POST['message'];
+
+    $email_from='govindasharma1014@gmail.com';
+    $email_subject="New form submission";
+    $email_body="User name:$name.\n".
+                    "User Email:$visitor_email.\n".
+                      "User Subject:$subject.\n".
+                       "User Message:$message.\n".
+
+$t0="arpitgupta1014@gmail.com";
+
+$headers="From: $email_from\r\n";
+$headers .="Reply-To: $visitor_email\r\n;
+mail($to,$email_subject,$email_body,$headers);
+header("Location: index.html");
+?>
